1281bff2e79bfd4a812f48de33cb7b2651cffc3a
Omansh Krishn <omansh@duck.com>
2025-05-30 14:36:53 +0530

    Revert "Install Qt to global prefix in Dockerfile"
    This reverts commit f0cfbacb4fa407a3205ac845de228ddb0422a9b4.

--- Telegram/build/qt_version.py
+++ Telegram/build/qt_version.py
@@ -5,9 +5,10 @@ def resolve(arch):
         os.environ['QT'] = '6.2.12'
     elif sys.platform == 'win32':
         if arch == 'arm' or 'qt6' in sys.argv:
-            print('Choosing Qt 6.')
             os.environ['QT'] = '6.9.0'
-        else:
-            print('Choosing Qt 5.')
+        elif os.environ.get('QT') is None:
             os.environ['QT'] = '5.15.15'
+    elif os.environ.get('QT') is None:
+        return False
+    print('Choosing Qt ' + os.environ.get('QT'))
     return True
